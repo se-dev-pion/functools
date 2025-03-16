acd838cc270645ef2ca61d5ab5c32386cc3d10fc
BioCrossCoder <viscaria_dignics@163.com>
2025-03-16 17:06:28 +0800

    [test] add templates for unit tests

---
+++ map_test.go
@@ -0,0 +1,23 @@
+package functools
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMap(t *testing.T) {
+	convey.Convey("slice", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+	convey.Convey("string", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+	convey.Convey("chan", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
---
+++ utils_test.go
@@ -0,0 +1,64 @@
+package functools
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDecorate(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestPack(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestLazy(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestPartial(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestPipe(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestBatch(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestCached(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
+
+func TestCopy(t *testing.T) {
+	convey.Convey("", t, func() {
+		var err error
+		assert.Nil(t, err)
+	})
+}
