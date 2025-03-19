f1bf11abc940c55c285d567e65f744dae39d33cc
BioCrossCoder <viscaria_dignics@163.com>
2025-03-19 22:45:43 +0800

    [test] add unit tests for Filter

--- filter_test.go
+++ filter_test.go
@@ -9,15 +9,41 @@ import (
 
 func TestFilter(t *testing.T) {
 	convey.Convey("slice", t, func() {
-		var err error
-		assert.Nil(t, err)
+		arr := []int{1, 2, 3, 4, 5}
+		f := func(x int) bool {
+			return x%2 == 0
+		}
+		assert.Equal(t, []int{2, 4}, Filter(f, arr)())
 	})
 	convey.Convey("string", t, func() {
-		var err error
-		assert.Nil(t, err)
+		seq := "golang"
+		f := func(x string) bool {
+			return x >= "g"
+		}
+		assert.Equal(t, "golng", Filter(f, seq)())
 	})
 	convey.Convey("chan", t, func() {
-		var err error
-		assert.Nil(t, err)
+		ch := make(chan int, 10)
+		for i := 1; i <= 5; i++ {
+			ch <- i
+		}
+		f := func(x int) bool {
+			return x%2 == 0
+		}
+		filtered := Filter(f, ch)()
+		nums := make([]int, 0)
+		for {
+			select {
+			case v, ok := <-filtered:
+				if !ok {
+					goto END
+				}
+				nums = append(nums, v)
+			default:
+				goto END
+			}
+		}
+	END:
+		assert.Equal(t, []int{2, 4}, nums)
 	})
 }
