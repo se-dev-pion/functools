097c321e861aaf56dc1617ab7b67ce8e30e6ef6c
BioCrossCoder <viscaria_dignics@163.com>
2025-03-22 14:58:50 +0800

    [test] add unit tests for Flow, Batch, Cached, Copy

--- utils_test.go
+++ utils_test.go
@@ -51,26 +51,79 @@ func TestPartial(t *testing.T) {
 	})
 }
 
-func TestPipe(t *testing.T) {
-	convey.Convey("pipe", t, func() {
-
+func TestFlow(t *testing.T) {
+	convey.Convey("flow", t, func() {
+		f1 := func(a int) int {
+			return a + 1
+		}
+		f2 := func(a int) int {
+			return a * 2
+		}
+		f3 := func(a int) int {
+			return a * a
+		}
+		assert.Equal(t, 16, Flow(f1, f2, f3)(1))
 	})
 }
 
 func TestBatch(t *testing.T) {
 	convey.Convey("batch", t, func() {
-
+		f1 := func(a int) int {
+			return a + 1
+		}
+		f2 := func(a int) int {
+			return a * 2
+		}
+		f3 := func(a int) int {
+			return a * a
+		}
+		assert.Equal(t, []int{2, 2, 1}, Batch(false, f1, f2, f3)(1))
+		assert.Equal(t, []int{2, 2, 1}, Batch(true, f1, f2, f3)(1))
 	})
+}
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
 }
 
 func TestCached(t *testing.T) {
 	convey.Convey("cache", t, func() {
-
+		f := Cached(fibonacci)
+		for i := 0; i <= 10; i++ {
+			assert.Equal(t, fibonacci(i), f(i))
+		}
 	})
 }
 
 func TestCopy(t *testing.T) {
 	convey.Convey("copy", t, func() {
-
+		convey.Convey("slice", func() {
+			arr := []int{1, 2, 3, 4, 5}
+			assert.Equal(t, arr, Copy[int](arr))
+		})
+		convey.Convey("chan", func() {
+			ch := make(chan int, 10)
+			for i := 1; i <= 5; i++ {
+				ch <- i
+			}
+			chCopy := Copy[int](ch)
+			nums := make([]int, 0)
+			for {
+				select {
+				case v, ok := <-chCopy:
+					if !ok {
+						goto END
+					}
+					nums = append(nums, v)
+				default:
+					goto END
+				}
+			}
+		END:
+			assert.Equal(t, []int{1, 2, 3, 4, 5}, nums)
+		})
 	})
 }
