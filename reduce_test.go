58898e12eff06edb3238749df1ea46d514c0ee70
BioCrossCoder <viscaria_dignics@163.com>
2025-03-23 11:17:02 +0800

    [test] add benchmark test for Reduce

--- reduce_test.go
+++ reduce_test.go
@@ -99,3 +99,114 @@ func ExampleReduce() {
 	// 15
 	// 21
 }
+
+func BenchmarkReduce(b *testing.B) {
+	// [slice]
+	{
+		arr := []int{1, 2, 3, 4, 5}
+		f := func(a, b int) int {
+			return a + b
+		}
+		b.Run("slice", func(b *testing.B) {
+			ff := Reduce(f, arr)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		b.Run("slice raw", func(b *testing.B) {
+			ff := Reduce4Slice(f, arr)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		// [with initial]
+		{
+			initial := 6
+			b.Run("slice with initial", func(b *testing.B) {
+				ff := Reduce(f, arr, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+			b.Run("slice raw with initial", func(b *testing.B) {
+				ff := Reduce4Slice(f, arr, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+		} // [/]
+	} // [/]
+	// [string]
+	{
+		seq := "golang"
+		f := func(a, b string) string {
+			return strings.ToUpper(a) + strings.ToUpper(b)
+		}
+		b.Run("string", func(b *testing.B) {
+			ff := Reduce(f, seq)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		b.Run("string raw", func(b *testing.B) {
+			ff := Reduce4String(f, seq)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		// [with initial]
+		{
+			initial := "GOOGLE"
+			b.Run("string with initial", func(b *testing.B) {
+				ff := Reduce(f, seq, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+			b.Run("string raw with initial", func(b *testing.B) {
+				ff := Reduce4String(f, seq, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+		} // [/]
+	} // [/]
+	// [chan]
+	{
+		ch := make(chan int, 10)
+		for i := 1; i <= 5; i++ {
+			ch <- i
+		}
+		f := func(a, b int) int {
+			return a + b
+		}
+		b.Run("chan", func(b *testing.B) {
+			ff := Reduce(f, ch)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		b.Run("chan raw", func(b *testing.B) {
+			ff := Reduce4Chan(f, ch)
+			for i := 0; i < b.N; i++ {
+				ff()
+			}
+		})
+		// [with initial]
+		{
+			initial := 6
+			b.Run("chan with initial", func(b *testing.B) {
+				ff := Reduce(f, ch, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+			b.Run("chan raw with initial", func(b *testing.B) {
+				ff := Reduce4Chan(f, ch, initial)
+				for i := 0; i < b.N; i++ {
+					ff()
+				}
+			})
+		} // [/]
+	} // [/]
+}
