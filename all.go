87a1f1c6e81f066875a728970ac31cc1fd436e99
BioCrossCoder <viscaria_dignics@163.com>
2025-03-23 10:34:16 +0800

    [feature] Expose internal implementation methods of All to public

--- all.go
+++ all.go
@@ -1,6 +1,6 @@
 package functools
 
-func all4Slice[T any, E ~[]T](condition FuncT2Bool[T], entry E) bool {
+func All4Slice[T any, E ~[]T](condition FuncT2Bool[T], entry E) bool {
 	for _, item := range entry {
 		if !condition(item) {
 			return false
@@ -9,7 +9,7 @@ func all4Slice[T any, E ~[]T](condition FuncT2Bool[T], entry E) bool {
 	return true
 }
 
-func all4String(condition FuncT2Bool[string], entry string) bool {
+func All4String(condition FuncT2Bool[string], entry string) bool {
 	for _, charCode := range entry {
 		if !condition(string(charCode)) {
 			return false
@@ -18,19 +18,23 @@ func all4String(condition FuncT2Bool[string], entry string) bool {
 	return true
 }
 
+func All4Chan[T any, E ~chan T](condition FuncT2Bool[T], entry E) bool {
+	return All4Slice(condition, extractChanElements(entry))
+}
+
 // All checks whether all elements in the given sequence(slice/chan/string) satisfy the specified condition.
 func All[T any, E Sequence[T] | ~string](condition FuncT2Bool[T], entry E) bool {
 	v := any(entry)
 	switch e := v.(type) {
 	case []T:
-		return all4Slice(condition, e)
+		return All4Slice(condition, e)
 	case string:
 		if _, ok := any(*new(T)).(string); !ok {
 			goto END
 		}
-		return all4String(any(condition).(FuncT2Bool[string]), e)
+		return All4String(any(condition).(FuncT2Bool[string]), e)
 	case chan T:
-		return all4Slice(condition, extractChanElements(e))
+		return All4Chan(condition, e)
 	}
 END:
 	return false
